bffc4725c318fad0d458b81950159deda947d177
nikitus1223 <105157074+nikitus1223@users.noreply.github.com>
2024-03-22 17:29:38 +0300

    1

---
+++ server.py
@@ -0,0 +1,123 @@
+from flask import Flask, request, jsonify
+import logging
+import json
+import random
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+
+cities = {
+    'москва': ['1030494/8d13c55193ebb496e0be',
+               '1030494/dedd6936c18028e5f45a'],
+    'нью-йорк': ['1030494/1e04407b0434a54f584e',
+                 '1652229/36aac5ab3a3acb5feae8'],
+    'париж': ['1652229/a64f9135e69b2a18e05e',
+              '1030494/7b519760e01f0175e11e'],
+    'тамбов': ['1652229/b7cc495bac9210913eda',
+               '997614/d404ea51874fea20ad91']
+}
+
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(response, request.json)
+    logging.info(f'Response: {response!r}')
+    return jsonify(response)
+
+
+def handle_dialog(res, req):
+    user_id = req['session']['user_id']
+    if req['session']['new']:
+        res['response']['text'] = 'Привет! Назови свое имя!'
+        sessionStorage[user_id] = {
+            'first_name': None
+        }
+        return
+
+    if sessionStorage[user_id]['first_name'] is None:
+        # в последнем его сообщение ищем имя.
+        first_name = get_first_name(req)
+        # если не нашли, то сообщаем пользователю что не расслышали.
+        if first_name is None:
+            res['response']['text'] = \
+                'Не расслышала имя. Повтори, пожалуйста!'
+        # если нашли, то приветствуем пользователя.
+        # И спрашиваем какой город он хочет увидеть.
+        else:
+            sessionStorage[user_id]['first_name'] = first_name
+            res['response'][
+                'text'] = 'Приятно познакомиться, ' \
+                          + first_name.title() \
+                          + '. Я - Алиса. Какой город хочешь увидеть?'
+            # получаем варианты buttons из ключей нашего словаря cities
+            res['response']['buttons'] = [
+                {
+                    'title': city.title(),
+                    'hide': True
+                } for city in cities
+            ]
+            res['response']['buttons'].append({
+                'title': 'помощь',
+                'hide': False
+            })
+    # если мы знакомы с пользователем и он нам что-то написал,
+    # то это говорит о том, что он уже говорит о городе,
+    # что хочет увидеть.
+    else:
+        # ищем город в сообщение от пользователя
+        city = get_city(req)
+        # если этот город среди известных нам,
+        # то показываем его (выбираем одну из двух картинок случайно)
+        if city == 'помощь':
+            res['response']['text'] = 'Данный навык представляет из себя мини игру.' \
+                                      ' Вы называете город, а я пытаюсь его отгадать,' \
+                                      ' возможно у меня не всегда получиться его угадать но я буду очень стараться'
+        elif city in cities:
+            res['response']['card'] = {}
+            res['response']['card']['type'] = 'BigImage'
+            res['response']['card']['title'] = 'Этот город я знаю.'
+            res['response']['card']['image_id'] = random.choice(cities[city])
+            res['response']['text'] = 'Я угадал!'
+        # если не нашел, то отвечает пользователю
+        # 'Первый раз слышу об этом городе.'
+        else:
+            res['response']['text'] = \
+                'Первый раз слышу об этом городе. Попробуй еще разок!'
+
+
+def get_city(req):
+    # перебираем именованные сущности
+    for entity in req['request']['nlu']['entities']:
+        # если тип YANDEX.GEO то пытаемся получить город(city),
+        # если нет, то возвращаем None
+        if entity['type'] == 'YANDEX.GEO':
+            # возвращаем None, если не нашли сущности с типом YANDEX.GEO
+            return entity['value'].get('city', None)
+        elif req['request']['command'] == 'помощь':
+            return 'помощь'
+
+
+def get_first_name(req):
+    # перебираем сущности
+    for entity in req['request']['nlu']['entities']:
+        # находим сущность с типом 'YANDEX.FIO'
+        if entity['type'] == 'YANDEX.FIO':
+            # Если есть сущность с ключом 'first_name',
+            # то возвращаем ее значение.
+            # Во всех остальных случаях возвращаем None.
+            return entity['value'].get('first_name', None)
+
+
+if __name__ == '__main__':
+    app.run()
